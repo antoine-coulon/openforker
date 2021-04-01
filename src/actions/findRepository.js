9f3e96ff1d216555a185872a4faf15d037b1d672
coulon_antoine <coulon_antoine@outlook.fr>
2021-04-02 01:32:54 +0200

    fix: findRepository test updated.

--- src/actions/findRepository.js
+++ src/actions/findRepository.js
@@ -1,9 +1,9 @@
-const { authenticate } = require('../auth/auth');
+const authModule = require('../auth/auth');
 const RepositoryNotFound = require('../errors/repositoryNotFound');
 const httpClient = require('../http/httpClient');
 
 async function findRepository(repositoryData) {
-  const { token } = await authenticate();
+  const { token } = await authModule.authenticate();
 
   const repository = await httpClient.findRepository({ ...repositoryData, token });
 
